01952be8f265d27ee4cbf5865fa0d1ad42d15ad9
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-04-16 17:48:06 +0200

    Upgrade to Spring Boot 2.2.0.M2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	val kotlinVersion = "1.3.30"
-	id("org.springframework.boot") version "2.2.0.BUILD-SNAPSHOT"
+	id("org.springframework.boot") version "2.2.0.M2"
 	id("org.jetbrains.kotlin.jvm") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
