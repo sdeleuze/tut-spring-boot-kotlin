b2f8ba051bdd427ba5f94bf4b95fe8c4326bf0a1
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-04-16 09:36:45 +0200

    Polishing

--- src/main/kotlin/com/example/blog/BlogProperties.kt
+++ src/main/kotlin/com/example/blog/BlogProperties.kt
@@ -3,6 +3,6 @@ package com.example.blog
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("blog")
-data class BlogProperties(val title: String, val banner: Banner)
-
-class Banner(val title: String?, val content: String)
+data class BlogProperties(val title: String, val banner: Banner) {
+	class Banner(val title: String?, val content: String)
+}
