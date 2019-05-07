2ee19b08938b4c3f4564d3ff139b8cf8c4f51ee7
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-05-07 18:23:16 +0200

    Update to leverage bodyFlowAndAwait

--- src/main/kotlin/com/example/blog/ArticleHandler.kt
+++ src/main/kotlin/com/example/blog/ArticleHandler.kt
@@ -3,11 +3,12 @@ package com.example.blog
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.bodyFlowAndAwait
 
 class ArticleHandler(private val repository: ArticleRepository) {
 
 	suspend fun findAll(request: ServerRequest) =
-			ok().bodyAndAwait(repository.findAll())
+			ok().bodyFlowAndAwait(repository.findAll())
 
 	suspend fun findOne(request: ServerRequest) =
 			ok().bodyAndAwait(repository.findBySlug(request.pathVariable("slug")) ?: throw IllegalArgumentException("Wrong article slug provided"))
--- src/main/kotlin/com/example/blog/CommentHandler.kt
+++ src/main/kotlin/com/example/blog/CommentHandler.kt
@@ -5,10 +5,11 @@ import kotlinx.coroutines.flow.flow
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.bodyFlowAndAwait
 
 class CommentHandler(private val repository: ArticleRepository) {
 
-	suspend fun findAllForArticle(request: ServerRequest) = ok().bodyAndAwait(flow {
+	suspend fun findAllForArticle(request: ServerRequest) = ok().bodyFlowAndAwait(flow {
 		delay(100)
 		emit(Comment("This is a comment from a slow remote HTTP endpoint", "Sébastien Deleuze"))
 		delay(100)
