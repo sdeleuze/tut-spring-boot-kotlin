5b03894b82fa55407337905f49f4a36848c35c9b
Sébastien Deleuze <sdeleuze@vmware.com>
2023-01-17 14:55:02 +0100

    Experiment with K2 compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	// K2 supported with Spring plugins only as of Kotlin 1.8.20 not released yet, see https://youtrack.jetbrains.com/issue/KT-53590
+	val kotlinVersion = "1.8.20-dev-5799"
 	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.0"
-	kotlin("plugin.spring") version "1.8.0"
-	kotlin("plugin.allopen") version "1.8.0"
-	kotlin("plugin.jpa") version "1.8.0"
-	kotlin("kapt") version "1.8.0"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+	kotlin("plugin.allopen") version kotlinVersion
+	kotlin("plugin.jpa") version kotlinVersion
+	// kapt is not supported with K2, see https://github.com/spring-projects/spring-boot/issues/28046 related Spring Boot issue
+	// kotlin("kapt") version kotlinVersion
 }
 
 group = "com.example"
@@ -15,6 +18,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
+	maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
 	mavenCentral()
 }
 
@@ -26,7 +30,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
-	kapt("org.springframework.boot:spring-boot-configuration-processor")
+	// kapt is not supported with K2, see https://github.com/spring-projects/spring-boot/issues/28046 related Spring Boot issue
+	// kapt("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(module = "mockito-core")
 	}
@@ -39,6 +44,7 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		jvmTarget = "17"
 		freeCompilerArgs += "-Xjsr305=strict"
+		useK2 = true
 	}
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
 	repositories {
+		maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
 		gradlePluginPortal()
 	}
 }
